421d07d9c97152f02999c34f4b0f85104becd1ab
AykhanMikayilzada <ayxan.mikayilzadee@gmail.com>
2024-06-07 12:37:27 +0400

    contact page updated

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -13,7 +13,15 @@ import {
   Textarea,
   Button,
   useToast,
+  Flex, // Flex bileşenini import et
+  Icon, // Icon bileşenini import et
 } from "@chakra-ui/react";
+import {
+  FaEnvelope,
+  FaInstagram,
+  FaLinkedin,
+  FaPhoneAlt,
+} from "react-icons/fa";
 
 function Contact() {
   const [firstName, setFirstName] = useState("");
@@ -31,7 +39,12 @@ function Contact() {
   const isMessageError = message === "";
 
   useEffect(() => {
-    if (!isFirstNameError && !isLastNameError && !isEmailError && !isMessageError) {
+    if (
+      !isFirstNameError &&
+      !isLastNameError &&
+      !isEmailError &&
+      !isMessageError
+    ) {
       setIsFormValid(true);
     } else {
       setIsFormValid(false);
@@ -46,7 +59,8 @@ function Contact() {
       console.log("Form submitted:", { firstName, lastName, email, message });
       toast({
         title: "Form submitted successfully.",
-        description: "You have successfully contacted us. We will get back to you via email shortly.",
+        description:
+          "You have successfully contacted us. We will get back to you via email shortly.",
         status: "success",
         duration: 5000,
         isClosable: true,
@@ -160,6 +174,53 @@ function Contact() {
           <Button colorScheme="teal" type="submit" isDisabled={!isFormValid}>
             Submit
           </Button>
+
+          <Text mt="20px" fontWeight="bold">
+            Contact Informations:
+          </Text>
+          <Flex alignItems="center" mb="10px">
+            <Icon as={FaEnvelope} mr="5px" />
+            <a href="mailto:info@proshop.az">
+              <Text _hover={{ textColor: "teal", cursor: "pointer" }}>
+                info@proshop.az
+              </Text>
+            </a>
+          </Flex>
+          <Flex alignItems="center" mb="10px">
+            <Icon as={FaInstagram} mr="5px" />
+            <a href="https://www.instagram.com/proshop.az/" target="blank">
+              <Text _hover={{ textColor: "teal" }}>@proshop.az</Text>
+            </a>
+          </Flex>
+          <Flex alignItems="center" mb="10px">
+            <Icon as={FaLinkedin} mr="5px" />
+            <a
+              href="https://www.linkedin.com/company/proshop-azerbaijan/"
+              target="blank"
+            >
+              <Text _hover={{ textColor: "teal" }}>
+                https://www.linkedin.com/company/proshop-azerbaijan/
+              </Text>
+            </a>
+          </Flex>
+          <Flex alignItems="center" mb="10px">
+            <Icon as={FaPhoneAlt} mr="5px" />
+            <a href="tel:+994509709996">
+              <Text _hover={{ textColor: "teal" }}>+994 50 970 99 96</Text>
+            </a>
+          </Flex>
+          <Flex alignItems="center" mb="10px"></Flex>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d759.4785552869035!2d49.8600493285276!3d40.41075101934726!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDDCsDI0JzM4LjciTiA0OcKwNTEnMzguNSJF!5e0!3m2!1saz!2saz!4v1717749298544!5m2!1saz!2saz"
+            title="Google Map"
+            width="100%"
+            height="450"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            aria-hidden="false"
+            tabIndex="0"
+          ></iframe>
+          <Flex alignItems="center" mb="10px"></Flex>
         </Box>
       </Box>
       <FooterSide />
