d75a886cbd9e25fca25b52846137524ad3445626
AykhanMikayilzada <ayxan.mikayilzadee@gmail.com>
2024-06-12 14:03:25 +0400

    some changes at all products

--- src/pages/Products/AllProducts/index.jsx
+++ src/pages/Products/AllProducts/index.jsx
@@ -1,123 +1,131 @@
-import React, { useEffect, useState } from 'react';
-import { Box, Text, Image } from '@chakra-ui/react';
-import { useTranslation } from 'react-i18next';
-import Header from '../../../components/Header';
-import FooterSide from '../../../components/FooterSide';
+import React, { useEffect, useState } from "react";
+import { Box, Text, Image } from "@chakra-ui/react";
+import { useTranslation } from "react-i18next";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Header from "../../../components/Header";
+import FooterSide from "../../../components/FooterSide";
 
 //Auto Branding
-import car1 from '../AutoBranding/imgs/car1.png';
-import car2 from '../AutoBranding/imgs/car2.png';
-import car3 from '../AutoBranding/imgs/car3.png';
-import car4 from '../AutoBranding/imgs/car4.png';
-import car5 from '../AutoBranding/imgs/car5.png';
-import car6 from '../AutoBranding/imgs/car6.png';
-import car7 from '../AutoBranding/imgs/car7.png';
-import car8 from '../AutoBranding/imgs/car8.png';
-import car9 from '../AutoBranding/imgs/car9.png';
-import car10 from '../AutoBranding/imgs/car10.png';
-import car11 from '../AutoBranding/imgs/car11.png';
+import car1 from "../AutoBranding/imgs/car1.png";
+import car2 from "../AutoBranding/imgs/car2.png";
+import car3 from "../AutoBranding/imgs/car3.png";
+import car4 from "../AutoBranding/imgs/car4.png";
+import car5 from "../AutoBranding/imgs/car5.png";
+import car6 from "../AutoBranding/imgs/car6.png";
+import car7 from "../AutoBranding/imgs/car7.png";
+import car8 from "../AutoBranding/imgs/car8.png";
+import car9 from "../AutoBranding/imgs/car9.png";
+import car10 from "../AutoBranding/imgs/car10.png";
+import car11 from "../AutoBranding/imgs/car11.png";
 
 //Promos
-import promo1 from '../Promo/imgs/promo1.png';
-import promo3 from '../Promo/imgs/promo3.png';
-import promo4 from '../Promo/imgs/promo4.png';
-import promo5 from '../Promo/imgs/promo5.png';
-import promo6 from '../Promo/imgs/promo6.png';
-import promo8 from '../Promo/imgs/promo8.png';
-import promo9 from '../Promo/imgs/promo9.png';
-import promo10 from '../Promo/imgs/promo10.png';
-import promo12 from '../Promo/imgs/promo12.png';
-import promo13 from '../Promo/imgs/promo13.png';
-import promo14 from '../Promo/imgs/promo14.png';
-import promo15 from '../Promo/imgs/promo15.png';
+import promo1 from "../Promo/imgs/promo1.png";
+import promo3 from "../Promo/imgs/promo3.png";
+import promo4 from "../Promo/imgs/promo4.png";
+import promo5 from "../Promo/imgs/promo5.png";
+import promo6 from "../Promo/imgs/promo6.png";
+import promo8 from "../Promo/imgs/promo8.png";
+import promo9 from "../Promo/imgs/promo9.png";
+import promo10 from "../Promo/imgs/promo10.png";
+import promo12 from "../Promo/imgs/promo12.png";
+import promo13 from "../Promo/imgs/promo13.png";
+import promo14 from "../Promo/imgs/promo14.png";
+import promo15 from "../Promo/imgs/promo15.png";
 
 //Shop Stands
-import shopStand1 from "../ShopStands/imgs/shopStand1.png"
-import shopStand2 from "../ShopStands/imgs/shopStand2.png"
-import shopStand3 from "../ShopStands/imgs/shopStand3.png"
-import polygraph1 from "../Polypraph/imgs/polygraph1.png"
-import polygraph2 from "../Polypraph/imgs/polygraph2.png"
-import polygraph3 from "../Polypraph/imgs/polygraph3.png"
+import shopStand1 from "../ShopStands/imgs/shopStand1.png";
+import shopStand2 from "../ShopStands/imgs/shopStand2.png";
+import shopStand3 from "../ShopStands/imgs/shopStand3.png";
+import polygraph1 from "../Polypraph/imgs/polygraph1.png";
+import polygraph2 from "../Polypraph/imgs/polygraph2.png";
+import polygraph3 from "../Polypraph/imgs/polygraph3.png";
 
 function AllProducts() {
-  const { t } = useTranslation(); 
+  const { t } = useTranslation();
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 768);
     };
 
     handleResize();
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
+  // Karışık sıralar
   const imageUrls = [
-    car1,
-    car2,
-    car3,
-    car4,
-    car5,
-    car6,
-    car7,
-    car8,
-    car9,
-    car10,
-    car11,
-    promo1,
-    promo3,
-    promo4,
-    promo5,
-    promo6,
-    promo8,
-    promo9,
-    promo10,
-    promo12,
-    promo13,
-    promo14,
     promo15,
-    shopStand1,
-    shopStand2,
-    shopStand3,
-    polygraph1,
     polygraph2,
-    polygraph3
+    car6,
+    promo3,
+    shopStand2,
+    car1,
+    promo9,
+    polygraph3,
+    shopStand1,
+    car9,
+    promo8,
+    car8,
+    promo12,
+    promo4,
+    shopStand3,
+    promo5,
+    car10,
+    car2,
+    car7,
+    promo10,
+    car4,
+    car11,
+    car5,
+    car3,
+    promo6,
+    polygraph1,
+    promo1,
+    promo14,
+    promo13,
   ];
 
   const texts = [
-    'Saray Çikiloo',
-    'Keçmişin Dadı',
-    'Bakı Süd',
-    'Mint Sənaye',
-    'Saray Choko Lips',
-    'Mint Sənaye',
-    'Mint Sənaye',
-    'Metro Park Lotoreya',
-    'Universal Magmaweld',
-    'Selenium',
-    'City Med',
-    'Qapı Lövhəsi',
-    'Papaq',
-    'Stəkan Altlığı',
-    'Not Dəfrəri',
-    'Powerbank',
-    'Qələm',
-    'Özəl Qutu',
-    'Termos',
-    'Dəftər Qələm',
-    'Kətan Çanta',
-    'Qələm',
-    'Kətan Çanta',
-    'Cheetos Stand',
-    'Lays Stand',
-    'Lays, Doritos, Xpyc Team Stand',
-    'Scalis Poliqrafiya',
-    'Sertifikat',
-    'Özəl Medallar',
+    "Qələm",
+    "Sertifikat",
+    "Mint Sənaye",
+    "Papaq",
+    "Lays Stand",
+    "Saray Çikiloo",
+    "Qələm",
+    "Özəl Medallar",
+    "Cheetos Stand",
+    "Universal Magmaweld",
+    "Qələm",
+    "Metro Park Lotoreya",
+    "Termos",
+    "Stəkan Altlığı",
+    "Lays, Doritos, Xpyc Team Stand",
+    "Not Dəfrəri",
+    "Selenium",
+    "Keçmişin Dadı",
+    "Mint Sənaye",
+    "Özəl Qutu",
+    "Mint Sənaye",
+    "City Med",
+    "Saray Choko Lips",
+    "Bakı Süd",
+    "Powerbank",
+    "Scalis Poliqrafiya",
+    "Qapı Lövhəsi",
+    "Kətan Çanta",
+    "Dəftər Qələm",
+    "Kətan Çanta",
   ];
 
   return (
@@ -136,12 +144,12 @@ function AllProducts() {
         <Text
           textColor="black"
           fontWeight="bold"
-          fontSize={{ base: '30px', sm: '40px' }}
+          fontSize={{ base: "30px", sm: "40px" }}
           w="100%"
           textAlign="center"
           pb="30px"
         >
-          {t('allProducts')} 
+          {t("allProducts")}
         </Text>
         <Box
           className="mainCard"
@@ -149,14 +157,15 @@ function AllProducts() {
           justifyContent="space-evenly"
           mt="50px"
           flexWrap="wrap"
-          gap={{ base: '10px', sm: '16px' }}
+          gap={{ base: "10px", sm: "16px" }}
           pb="50px"
+          data-aos="fade"
         >
           {imageUrls.map((url, index) => (
             <Box
               key={index}
               className="card"
-              w={{ base: '240px', sm: '288px' }}
+              w={{ base: "240px", sm: "288px" }}
               h="200px"
               boxShadow="rgba(0, 0, 0, 0.1) 0px 4px 12px, rgba(0, 0, 0, 0.1) 0px 6px 24px"
               bg="white"
@@ -165,9 +174,9 @@ function AllProducts() {
               flexDir="column"
               alignItems="center"
               pos="relative"
-              mt={!isMobile ? '100px' : '70px'}
+              mt={!isMobile ? "100px" : "70px"}
               overflow="visible"
-              _hover={{ cursor: 'pointer' }}
+              _hover={{ cursor: "pointer" }}
             >
               <Image
                 src={url}
@@ -178,7 +187,7 @@ function AllProducts() {
                 w="auto"
                 h="140px"
                 transition="transform 0.3s ease"
-                _hover={{ transform: 'translateX(-50%) scale(1.1)' }}
+                _hover={{ transform: "translateX(-50%) scale(1.1)" }}
               />
               <Box
                 display="flex"
@@ -189,7 +198,7 @@ function AllProducts() {
                 <Text
                   textColor="black"
                   fontWeight="bold"
-                  fontSize={{ base: '16px', sm: '18px' }}
+                  fontSize={{ base: "16px", sm: "18px" }}
                 >
                   {texts[index]}
                 </Text>
